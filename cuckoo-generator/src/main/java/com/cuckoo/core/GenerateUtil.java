00687e6b9931a486d272091f83c9eff865de2403
Jasper <tomato6.jc@gmail.com>
2020-05-29 09:14:30 +0800

    Add GenerateUtil.java

---
+++ cuckoo-generator/src/main/java/com/cuckoo/core/GenerateUtil.java
@@ -0,0 +1,13 @@
+package com.cuckoo.core;
+
+public class GenerateUtil {
+    private String base = System.getProperty("user.dir") + "\\src";// user.dir得到的是项目的绝对路径
+
+    private String baseUrlStr = Constant.baseUrlStr;
+    private String utilsStr = Constant.utilsStr;
+    private String controllerStr = Constant.controllerStr;
+    private String serviceStr = Constant.serviceStr;
+    private String entityStr = Constant.entityStr;
+    private String implStr = Constant.implStr;
+    private String daoStr = Constant.daoStr;
+}
